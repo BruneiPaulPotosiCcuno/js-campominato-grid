5205ca8d11bf8b89e3ffb58441898d641186ab17
Brunei Paul Potosi Ccuno <neoxtrotoka@gmail.com>
2024-03-18 00:40:41 +0100

    add-grid-with-btn

--- js/script.js
+++ js/script.js
@@ -6,3 +6,14 @@
 
 //prendiamo il btn del html
 const btn = document.querySelector('#btn');
+
+//creo l'evento listener per il btn
+btn.addEventListener('click', function() {
+    //Creo la griglia per i numeri
+    const mainGrid = document.querySelector('#grid');
+    for (let i = 1; i <= 100; i++){
+        const newSquare = generateSquare(i);
+        //aggiungo square allla griglia
+        mainGrid.appendChild(newSquare);
+    }
+});
